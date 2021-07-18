4517e137e5db8667e9c69a85bfddec8d490363ee
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-18 16:09:05 -0700

    Begin writing test to move method in controller to model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  it { should have_many(:items) }
+  describe 'relationships' do
+    it { should have_many(:items) }
+  end
+
+  describe 'class methods' do
+    xit 'can give a limit of merchants shown per page' do
+      expect(Merchant.page_limit(20)).to eq(20)
+    end
+  end
 end
