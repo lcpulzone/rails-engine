c7990d15f3bebd7791df16c0329fed2bcc05a991
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-18 15:07:56 -0700

    Remove commented out lines, take out pry

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,18 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Get Merchants', type: :request do
   it 'can get all merchants' do
-    #when adding about serialzier might need to adjust the test
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
     merchants = JSON.parse(response.body, symbolize_names: true)
 
-# expect(response.status).to eq(200)
     expect(response).to be_successful
+    expect(response.status).to eq(200)
     expect(merchants[:data].count).to eq(3)
     expect(merchants[:data].class).to eq(Array)
     expect(merchants[:data].first[:attributes]).to have_key(:name)
     expect(merchants[:data].first[:attributes][:name]).to be_a(String)
-    require "pry";binding.pry 
   end
 end
