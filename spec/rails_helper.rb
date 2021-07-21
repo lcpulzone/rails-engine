ea97ca0494b524f7d84f0ef2d1360fe2bf4862b6
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-20 19:32:05 -0700

    Add in files to be ignored by SimpleCov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 
 require 'spec_helper'
+SimpleCov.start 'rails' do
+  add_filter ['/app/mailers', '/app/jobs', 'app/channels']
+end
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
