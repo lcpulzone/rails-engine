2372726e0b6cb15bfaa0545e33e08d1f6cb66f92
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-20 21:24:55 -0700

    Add create and delete spec

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -77,4 +77,26 @@ RSpec.describe 'Items Request' do
       expect(item[:data][:attributes][:unit_price].class).to eq(Float)
     end
   end
+
+  describe 'destroy' do
+    it 'can destroy an item' do
+      merchant6 = create(:merchant)
+      item_params = {
+        name: "Llama",
+        description: "Cute animal, not a good pet",
+        unit_price: 5.99,
+        merchant_id: "#{merchant6.id}"
+      }
+
+      post '/api/v1/items', params: item_params
+
+      item = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
+      delete "/api/v1/items/#{item[:data][:id]}"
+
+      expect(response.status).to eq(204)
+    end
+  end
 end
