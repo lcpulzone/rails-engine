7dc0df6d813407c6ca64f936115cbf1b4c98eb6f
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-20 17:54:41 -0700

    Clean up items spec, add merchants spec

--- spec/requests/api/v1/merchants/items_request_spec.rb
+++ spec/requests/api/v1/merchants/items_request_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Merchant Items' do
   describe 'all items for a merchant' do
     it 'can show all items for a given merchant' do
-      @merchant2 = create(:merchant)
-      @items = create_list(:item, 25, merchant: @merchant2)
+      merchant2 = create(:merchant)
+      items = create_list(:item, 25, merchant: merchant2)
 
-      get api_v1_merchant_items_path("#{@merchant2.id}")
+      get api_v1_merchant_items_path("#{merchant2.id}")
       merchant = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
