577bcd5914607b4403d38b3b75cb5368e7afe2e8
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-22 15:08:13 -0700

    Update routes with non-restful route

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,24 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show, :create, :destroy] do
+      resources :items, only: [:index, :show, :create, :update, :destroy] do
         resource :merchant, module: 'items', only: [:show]
       end
+      get '/merchants/find', to: 'merchants#search'
       resources :merchants, only: [:index, :show] do
         resources :items, module: 'merchants', only: [:index]
       end
     end
   end
+  # namespace :api do
+  #   namespace :v1 do
+  #     namespace :merchants do
+  #       resource :find, controller: :search_merchant, only: :show
+  #     end
+  #   end
+  # end
 end
 
-# namespace :api do
-#   namespace :v1 do
-#     namespace :merchants do
-#       resource :find, controller: :search_merchant, only: :show
-#     end
 #     scope module: :merchants do
 #       resources :merchants, controller: :resources do
 #         resources :items, controller: :merchant_items, only: :index
