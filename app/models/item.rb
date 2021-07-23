84fa80387e7ff817f5853f8e55f996eff79ba83b
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-23 00:43:25 -0700

    Comment out unused tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,12 +5,12 @@ class Item < ApplicationRecord
   # has_many :transactions, through: :invoices
   # has_many :customers, through: :invoices
 
-  def self.items_by_revenue(number)
-    joins(invoice_items: {invoice: :transactions})
-    .where("invoices.status='shipped' AND transactions.result='success'")
-    .select("items.*, sum(invoice_items.quantity * invoice_items.unit_price) as total_revenue")
-    .group("items.id")
-    .order("total_revenue DESC")
-    .limit(number)
-  end
+  # def self.items_by_revenue(number)
+  #   joins(invoice_items: {invoice: :transactions})
+  #   .where("invoices.status='shipped' AND transactions.result='success'")
+  #   .select("items.*, sum(invoice_items.quantity * invoice_items.unit_price) as total_revenue")
+  #   .group("items.id")
+  #   .order("total_revenue DESC")
+  #   .limit(number)
+  # end
 end
