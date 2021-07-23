123f0f873447d382424f6523fc946c0c29eadda3
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-22 20:19:25 -0700

    Create model spec for all relationships

---
+++ spec/models/invoice_item_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe InvoiceItem, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:invoice) }
+    it { should belong_to(:item) }
+    it { should have_many(:customers).through(:invoice) }
+    it { should have_many(:transactions).through(:invoice) }
+    it { should have_many(:merchants).through(:item) }
+  end
+end
