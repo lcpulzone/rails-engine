6e9671ed431ddcbdf353d503d8b6d02ed1398963
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-07-22 20:21:01 -0700

    Create models and set up relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,11 +1,7 @@
-class Merchant < ApplicationRecord
-  has_many :items
+class Customer < ApplicationRecord
   has_many :invoices
+  has_many :transactions, through: :invoices
   has_many :invoice_items, through: :invoices
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
-
-  def self.find_name(name)
-    Merchant.where("name ILIKE ?" ,"%#{name}%").limit(1)
-  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
+
+  def self.items_by_revenue
+    joins(invoice_items: {invoice: :transactions})
+    .where("invoice.status='shipped' AND transactions.result='success'")
+    .group("items.id")
+    .select("items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .order("revenue DESC")
+    .limit(10)
+  end
 end
